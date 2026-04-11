22c0536b0a878fe700fe1823eaaebed509cb97c7
dancoopper <samuellucanbrowne@gmail.com>
2026-04-11 13:33:58 -0400

    wk 13

---
+++ wk13/getusers_1.php
@@ -0,0 +1,32 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "lab9");
+
+$results = [];
+if (isset($_GET["query"])) {
+    $name = $_GET["query"];
+    $sql = "SELECT * FROM users WHERE firstname = '$name' AND active = 1";
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        $results[] = $row;
+    }
+}
+?>
+
+<form method="GET">
+    <input type="text" name="query" placeholder="Search by first name">
+    <button type="submit">Search</button>
+</form>
+
+<table border="1">
+    <tr><th>ID</th><th>Username</th><th>Email</th><th>First</th><th>Last</th><th>Active</th></tr>
+    <?php foreach ($results as $row): ?>
+    <tr>
+        <td><?= $row['id'] ?></td>
+        <td><?= $row['username'] ?></td>
+        <td><?= $row['email'] ?></td>
+        <td><?= $row['firstname'] ?></td>
+        <td><?= $row['lastname'] ?></td>
+        <td><?= $row['active'] ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
